2e47f844de06f44aef7d35af2ec54bff352c5921
Genesis Murillo <pair+genesis@spacecamp.ninja>
2020-04-28 08:45:11 -0600

    Changed layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import './App.scss';
-import Navigation from './components/Navigation/Navigation';
 import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { Resume, Contact, About } from './pages/index';
+
+export const NavLogo = () => {
+  return(
+      <div className="nav-logo">
+          Genesis
+      </div>
+  );
+};
 
 const App = () => {
 
   return (
     <div className="App">
-      <Navigation />
+      <Router>
+        <nav>
+          <NavLogo />
+          <div className="nav-links">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
+        </nav>
+        <Container id="main">
+          <Switch>
+            <Route exact path="/">
+              <Resume />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
       <Footer />
     </div>
   );
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Title from '../../components/Title/Title';
+import { Row, Col, Image } from 'react-bootstrap';
+import Avatar from '../../images/avatar.png';
 
 const About = () => {
     return(
-        <Title>About</Title>
+        <Fragment>
+            <Title>About</Title>
+            <Row>
+                <Col className="text-center">
+                    <p>I am 27-years-old.</p>
+                </Col>
+            </Row>
+        </Fragment>
+        
     );
 }
 
